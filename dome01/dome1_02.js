5ab638f13e2c0d863719ebfdaf889b0b07604914
Braveheart-qw <609830076@qq.com>
2020-06-27 19:59:50 +0800

    知识补充

--- dome01/dome1_02.js
+++ dome01/dome1_02.js
@@ -1,10 +1,16 @@
 window.onload = function () {
-   var array = ["钢铁侠" , "美队" , "雷神索尔"];
-   var newArray = array.splice(3,0,"金刚狼","??");
-   alert(newArray);
-   alert(array);
+   var temp = null;
+   var array = [9 , 6 , 7 , 15 , 8 , 3];
 
+      for(var i = 0;i < array.length-1 ; i++)
+         for(var j = i+1 ; j<=array.length; j++)
+            if(array[i] > array[j]){
+               array[i] = array[i] + array[j];
+               array[j] = array[i] - array[j];
+               array[i] = array[i] - array[j];
+            }
 
+      alert(array);
 }
 
 
