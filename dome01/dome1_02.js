732e6dc2e22f201fc68413c38d78289a640ada0d
Braveheart-qw <609830076@qq.com>
2020-07-07 21:37:11 +0800

    学习

--- dome01/dome1_02.js
+++ dome01/dome1_02.js
@@ -1,9 +1,16 @@
 window.onload = function () {
 
-    var date = new Date("2020-05-3");
-    alert(date);
+    var array = [10,20,30,40,50];
 
+    function sum(i){
+        if (i == 0){
+            return array[i];
+        }else{
+            return sum(i-1)+array[i];
+        }
+    }
 
+   alert( sum(array.length-1));
 
 }
 
